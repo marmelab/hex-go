c98da0d60b7d3383967fe3de8a464ccbbc26299b
Chroq <christophe.lecroq@gmail.com>
2019-12-04 12:22:20 +0100

    Add Player1 victory detection

--- src/hex/graph.go
+++ src/hex/graph.go
@@ -1,16 +1,54 @@
 package main
 
 import (
+	"fmt"
 	"github.com/RyanCarrier/dijkstra"
+	"math"
 )
 
-func buildGraph(stones []Stone, player int) *dijkstra.Graph {
-	graph := dijkstra.NewGraph()
+const StartVertexId = 0
 
+func buildGraph(stones []Stone, player int, width int) *dijkstra.Graph {
+	graph := dijkstra.NewGraph()
+	endVertexId := width*width + 1
+
+	graph.AddVertex(StartVertexId)
 	addVertexToGraph(stones, graph, player)
+	graph.AddVertex(endVertexId)
+
+	addStartArcsToGraph(stones, graph, player, width)
 	addArcsToGraph(stones, graph, player)
+	addEndArcsToGraph(stones, graph, player, width, endVertexId)
 
 	return graph
+}
+
+func addStartArcsToGraph(stones []Stone, graph *dijkstra.Graph, player int, width int) {
+	if player == Player1 {
+		for _, stone := range stones[:width] {
+			// @todo: Check if there's another (clearer) way to do.
+			distance := int64(math.Abs(float64(stone.player - 1)))
+
+			if nil != graph.AddArc(StartVertexId, stone.id, distance) {
+				fmt.Errorf("error during Arc insertion")
+			}
+		}
+	}
+}
+
+func addEndArcsToGraph(stones []Stone, graph *dijkstra.Graph, player int, width int, EndVertexId int) {
+	if player == Player1 {
+
+		offset := (width * width) - width
+		for _, stone := range stones[offset:] {
+			// @todo: Check if there's another (clearer) way to do.
+			distance := int64(math.Abs(float64(stone.player - 1)))
+
+			if nil != graph.AddArc(stone.id, EndVertexId, distance) {
+				fmt.Errorf("error during Arc insertion")
+			}
+		}
+	}
 }
 
 func addArcsToGraph(stones []Stone, graph *dijkstra.Graph, player int) {
--- src/hex/graph_test.go
+++ src/hex/graph_test.go
@@ -12,8 +12,9 @@ func TestUserCanBuildAGraphBasedOnStonesArray(t *testing.T) {
 		{2, 1, 0},
 	}
 
+	width := len(matrix)
 	stones := getStonesFromMatrix(matrix)
-	graph := buildGraph(stones, Player1)
+	graph := buildGraph(stones, Player1, width)
 
 	path := []int{2, 5, 8, 9}
 	expected := dijkstra.BestPath{Distance: int64(1), Path: path}
@@ -24,4 +25,35 @@ func TestUserCanBuildAGraphBasedOnStonesArray(t *testing.T) {
 		t.Errorf("got %d; want %d", got.Distance, expected.Distance)
 	}
 
+}
+
+func TestUserCanDetectVictoryOfPlayer1(t *testing.T) {
+	matrix := [][]int{
+		{0, 1, 0},
+		{2, 1, 0},
+		{2, 1, 0},
+	}
+
+	width := len(matrix)
+	endVertexId := (width * width) + 1
+
+	stones := getStonesFromMatrix(matrix)
+	graph := buildGraph(stones, Player1, width)
+
+	path := []int{StartVertexId, 2, 5, 8, endVertexId}
+	expected := dijkstra.BestPath{Distance: int64(0), Path: path}
+
+	got, _ := graph.Shortest(StartVertexId, endVertexId)
+
+	if got.Distance != expected.Distance {
+		t.Errorf("got %d; want %d", got.Distance, expected.Distance)
+	}
+
+	for i, path := range expected.Path {
+		{
+			if path != got.Path[i] {
+				t.Errorf("got %d; want %d", path, got.Path[i])
+			}
+		}
+	}
 }
