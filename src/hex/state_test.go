13f6f2aefccd4e744adbfd6db4a2afa0e51b37cf
Chroq <christophe.lecroq@gmail.com>
2019-12-05 09:42:16 +0100

    Determine the closest player to victory

--- src/hex/state_test.go
+++ src/hex/state_test.go
@@ -57,3 +57,43 @@ func TestUserCanKnowIfTheGameIsWon(t *testing.T) {
 		t.Errorf("got %t; want true", got)
 	}
 }
+
+func TestUserCanKnowIfHeIsClosestThanOpponent(t *testing.T) {
+
+	matrix := [][]int{
+		{1, 1, 0},
+		{2, 0, 0},
+		{2, 1, 0},
+	}
+	graph, endVertexId := bootstrapGraphPlayer1(matrix)
+	bestPath, _ := graph.Shortest(StartVertexId, endVertexId)
+
+	graphOpponent, endVertexIdOpponennt := bootstrapGraphPlayer2(matrix)
+	bestPathOpponent, _ := graphOpponent.Shortest(StartVertexId, endVertexIdOpponennt)
+
+	got := IsClosestAsOpponent(bestPath, bestPathOpponent)
+
+	if got != true {
+		t.Errorf("got %t; want true", got)
+	}
+}
+
+func TestUserCanKnowIfHeIsNotClosestThanOpponent(t *testing.T) {
+
+	matrix := [][]int{
+		{1, 1, 0},
+		{2, 2, 0},
+		{2, 1, 0},
+	}
+	graph, endVertexId := bootstrapGraphPlayer1(matrix)
+	bestPath, _ := graph.Shortest(StartVertexId, endVertexId)
+
+	graphOpponent, endVertexIdOpponennt := bootstrapGraphPlayer2(matrix)
+	bestPathOpponent, _ := graphOpponent.Shortest(StartVertexId, endVertexIdOpponennt)
+
+	got := IsClosestAsOpponent(bestPath, bestPathOpponent)
+
+	if got != false {
+		t.Errorf("got %t; want false", got)
+	}
+}
