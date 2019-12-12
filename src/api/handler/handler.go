1d06fd42e1e6ce3db89b62f8e6bd39d293dbc9de
Chroq <christophe.lecroq@gmail.com>
2019-12-12 10:45:06 +0100

    Fix on redondant instruction

--- src/api/handler/handler.go
+++ src/api/handler/handler.go
@@ -18,18 +18,12 @@ func IsWonHandler(responseWriter http.ResponseWriter, request *http.Request) {
 	currentGame := getGameByRequest(request)
 	response := fmt.Sprintf(`{"isWon": %t}`, game.IsWinningGame(currentGame))
 
-	prepareHeaders(&responseWriter)
 	responseWriter.WriteHeader(http.StatusOK)
-
 	io.WriteString(responseWriter, response)
 }
 
-func prepareHeaders(writer *http.ResponseWriter) {
-	(*writer).Header().Set("Content-Type", "application/json")
-	(*writer).Header().Set("Access-Control-Allow-Origin", "*")
-}
-
 func setupResponse(writer *http.ResponseWriter) {
+	(*writer).Header().Set("Content-Type", "application/json")
 	(*writer).Header().Set("Access-Control-Allow-Origin", "*")
 	(*writer).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	(*writer).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
